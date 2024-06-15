08b4d2a0d44c1ed23ada382d12ca9e36dfd102af
Sid1st <97543215+Sid1st@users.noreply.github.com>
2024-06-15 19:23:06 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,7 +4,7 @@ from os import getenv, environ
 
 
 # Online Stream and Download
-PORT = int(environ.get('PORT', 8080))
+PORT = int(environ.get('PORT', 5000))
 NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
